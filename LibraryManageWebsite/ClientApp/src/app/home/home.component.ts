5b578ceafcc1ffba23c82543c40b9c292019032d
AronChen <AronChen@XA-DD3-ARON>
2020-03-29 17:51:36 +0800

    change home use controller to get data

--- LibraryManageWebsite/ClientApp/src/app/home/home.component.ts
+++ LibraryManageWebsite/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  stus: Array<Student>;
+  constructor(http: HttpClient, @Inject("BASE_URL") baseUrl: string) {
+    http.get<Array<Student>>(baseUrl + "Students").subscribe(result => {
+      this.stus = result;
+    });
+  }
+}
+
+
+interface Student {
+  name: string;
+  age: number;
+  address: string;
+  hobby: string;
 }
